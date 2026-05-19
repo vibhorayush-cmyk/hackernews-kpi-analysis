4ac7102901e15ef43219acb5867a1bd6e5052374
vibhorayush-cmyk <vibhorayush@gmail.com>
2026-05-19 07:07:01 +0530

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ import pandas as pd
 from data_cleaning import clean_data
 from feature_engineering import engineer_features
 from kpi_analysis import generate_kpis
+
 from visualization import (
     create_top_posts_chart,
-    create_engagement_distribution
+    create_engagement_distribution,
+    create_title_length_analysis
 )
 
 # Load dataset
@@ -14,28 +16,32 @@ df = pd.read_csv(
     r'C:\Users\nehad\OneDrive\news-kpi\data\raw\hackernews_final.csv'
 )
 
-# Run pipeline
+# Clean data
 df = clean_data(df)
 
+# Feature engineering
 df = engineer_features(df)
 
+# KPI analysis
 generate_kpis(df)
 
+# Visualizations
 create_top_posts_chart(df)
 
 create_engagement_distribution(df)
 
-# Save cleaned data
+create_title_length_analysis(df)
 
-
+# Create cleaned folder if missing
 os.makedirs(
     'data/cleaned',
     exist_ok=True
 )
 
+# Export cleaned dataset
 df.to_csv(
     'data/cleaned/hackernews_cleaned.csv',
     index=False
 )
 
-print("Pipeline completed successfully.")
+print("\nPipeline completed successfully.")
---
+++ src/visualization.py
@@ -0,0 +1,85 @@
+import os
+import matplotlib.pyplot as plt
+
+os.makedirs('visuals', exist_ok=True)
+
+
+def create_top_posts_chart(df):
+
+    top_posts = (
+        df.sort_values(
+            by='points',
+            ascending=False
+        )
+        .head(10)
+    )
+
+    plt.figure(figsize=(12, 6))
+
+    plt.barh(
+        top_posts['title'],
+        top_posts['points']
+    )
+
+    plt.xlabel('Points')
+
+    plt.title(
+        'Top Performing Hacker News Posts'
+    )
+
+    plt.tight_layout()
+
+    plt.savefig(
+        'visuals/top_posts.png'
+    )
+
+    plt.close()
+
+
+def create_engagement_distribution(df):
+
+    plt.figure(figsize=(8, 5))
+
+    df['engagement_level'].value_counts().plot(
+        kind='bar'
+    )
+
+    plt.title(
+        'Engagement Level Distribution'
+    )
+
+    plt.xlabel('Engagement Level')
+    plt.ylabel('Number of Posts')
+
+    plt.tight_layout()
+
+    plt.savefig(
+        'visuals/engagement_distribution.png'
+    )
+
+    plt.close()
+
+
+def create_title_length_analysis(df):
+
+    plt.figure(figsize=(10, 6))
+
+    plt.scatter(
+        df['title_length'],
+        df['points']
+    )
+
+    plt.xlabel('Title Length')
+    plt.ylabel('Points')
+
+    plt.title(
+        'Title Length vs Engagement'
+    )
+
+    plt.tight_layout()
+
+    plt.savefig(
+        'visuals/title_length_analysis.png'
+    )
+
+    plt.close()
